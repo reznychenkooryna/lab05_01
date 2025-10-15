8bbc7903f9e419ad6260331cf67530792d491207
reznychenkooryna <reznychenkooryna@gmail.com>
2025-10-15 21:27:52 +0300

    first master commit

---
+++ Lab_05_01/Lab_05_01.cpp
@@ -0,0 +1,25 @@
+﻿// Lab 5_1
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+double g(const double x, const double y);  // прототип 
+
+int main()
+{
+    double r, s;
+    cout << "r = "; cin >> r;
+    cout << "s = "; cin >> s;
+
+    double c = (pow(g(r, s * s + 1), 2) + g(1, r * r + s)) / (1 + pow(g(1, r * s), 2));
+
+    cout << "c = " << c << endl;
+
+    return 0;
+}
+
+double g(const double x, const double y)  // визначення 
+{
+    return (sin(x) * sin(x) + cos(y) * cos(y)) / (1 + fabs(x * y));
+}
